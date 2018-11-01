77272ecea45f4b407b5378b77b91216b99a46b9a
grassknoted <grassknoted@gmail.com>
2018-11-02 02:24:59 +0530

    design changes

--- app/src/main/java/com/example/sameer/mindinstall/Usermain.java
+++ app/src/main/java/com/example/sameer/mindinstall/Usermain.java
@@ -1,13 +1,34 @@
 package com.example.sameer.mindinstall;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.textclassifier.TextClassification;
 
-public class Usermain extends AppCompatActivity {
+import com.google.android.gms.common.api.Response;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+import static android.provider.Telephony.Carriers.PASSWORD;
+
+public class Usermain extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usermain);
     }
+
 }
